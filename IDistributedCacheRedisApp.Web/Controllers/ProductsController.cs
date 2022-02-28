c99a49a0467a47492a708d961afd8ac2921afb3f
iskylarkus <turgay.kibar@gmail.com>
2022-02-28 15:29:48 +0300

    set get and remove actions for product controller developed

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -12,8 +12,48 @@ namespace IDistributedCacheRedisApp.Web.Controllers
             _distributedCache = distributedCache;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            
+            distributedCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            distributedCacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(20);
+
+            _distributedCache.SetString("name", "ilker", distributedCacheEntryOptions);
+
+            await _distributedCache.SetStringAsync("surname", "selvi", distributedCacheEntryOptions);
+
+            return View();
+        }
+
+        public async Task<IActionResult> Show()
+        {
+            string name = _distributedCache.GetString("name");
+
+            if (String.IsNullOrEmpty(name))
+            {
+                name = "NONE";
+            }
+
+            ViewBag.name = name;
+
+            string surname = await _distributedCache.GetStringAsync("surname");
+
+            if (String.IsNullOrEmpty(surname))
+            {
+                surname = "NONE";
+            }
+
+            ViewBag.surname = surname;
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
             return View();
         }
     }
