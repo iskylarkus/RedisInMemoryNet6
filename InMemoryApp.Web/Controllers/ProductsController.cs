749e2fc8bf769a9c989245319f676061ca151e86
iskylarkus <turgay.kibar@gmail.com>
2022-02-28 10:24:38 +0300

    imemorycache various methods samples

--- InMemoryApp.Web/Controllers/ProductsController.cs
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -14,6 +14,21 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
+            // yoksa set et ilk yöntem
+            if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+            
+            // al ve out ile ver yoksa set et ikinci yöntem
+            if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            ViewBag.zaman = zamanCache;
+
+            // direk kontrol etmeden oluştur
             _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
 
             return View();
@@ -21,7 +36,18 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Show()
         {
+            // cache ten al
             ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            
+            // cache ten sil
+            _memoryCache.Remove("zaman");
+
+            // varsa al yoksa oluştur
+            _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+            
             return View();
         }
     }
