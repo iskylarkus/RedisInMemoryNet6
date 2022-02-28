10fd672f1e6c32531c103c333ff1904b1574c33f
iskylarkus <turgay.kibar@gmail.com>
2022-02-28 17:36:36 +0300

    redis service helper class implemented

--- RedisExchangeAPI.Web/Program.cs
+++ RedisExchangeAPI.Web/Program.cs
@@ -1,6 +1,11 @@
+using RedisExchangeAPI.Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<RedisService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -12,6 +17,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
+RedisService redisService = app.Services.GetRequiredService<RedisService>();
+redisService.Connect();
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
