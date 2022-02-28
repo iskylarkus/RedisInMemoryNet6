9e0ef8136dc820494e2002213cd387e0164e199e
iskylarkus <turgay.kibar@gmail.com>
2022-02-28 15:52:30 +0300

    product complex type as json string caching

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -53,6 +55,46 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         public IActionResult Remove()
         {
             _distributedCache.Remove("name");
+
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+
+            distributedCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(10);
+
+            distributedCacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(100);
+
+            Product product1 = new Product { Id = 1, Name = "Kalem", Price = 38 };
+            Product product2 = new Product { Id = 2, Name = "Silgi", Price = 19 };
+            Product product3 = new Product { Id = 3, Name = "Kitap", Price = 95 };
+
+            string jsonProduct1 = JsonSerializer.Serialize(product1);
+            string jsonProduct2 = JsonSerializer.Serialize(product2);
+            string jsonProduct3 = JsonSerializer.Serialize(product3);
+
+            await _distributedCache.SetStringAsync($"{nameof(Product)}:{product1.Id}", jsonProduct1, distributedCacheEntryOptions);
+            await _distributedCache.SetStringAsync($"{nameof(Product)}:{product2.Id}", jsonProduct2, distributedCacheEntryOptions);
+            await _distributedCache.SetStringAsync($"{nameof(Product)}:{product3.Id}", jsonProduct3, distributedCacheEntryOptions);
+
+            return View();
+        }
+
+        public async Task<IActionResult> List()
+        {
+            string jsonProduct1 = await _distributedCache.GetStringAsync("Product:1");
+            string jsonProduct2 = await _distributedCache.GetStringAsync("Product:2");
+            string jsonProduct3 = await _distributedCache.GetStringAsync("Product:3");
+
+            Product product1 = JsonSerializer.Deserialize<Product>(jsonProduct1);
+            Product product2 = JsonSerializer.Deserialize<Product>(jsonProduct2);
+            Product product3 = JsonSerializer.Deserialize<Product>(jsonProduct3);
+
+            ViewBag.product1 = product1;
+            ViewBag.product2 = product2;
+            ViewBag.product3 = product3;
 
             return View();
         }
