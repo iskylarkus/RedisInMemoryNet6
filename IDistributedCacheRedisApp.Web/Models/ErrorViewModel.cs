6f8d24b365a4a4f99684b8e4c236b5e4ac7a3adf
iskylarkus <turgay.kibar@gmail.com>
2022-02-28 17:00:45 +0300

    small changes

--- IDistributedCacheRedisApp.Web/Models/ErrorViewModel.cs
+++ IDistributedCacheRedisApp.Web/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace IDistributedCacheRedisApp.Web.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
